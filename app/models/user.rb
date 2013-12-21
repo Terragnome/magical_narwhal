f4cb881cc4c7c31369cef85982caf60969412b67
mlin <mlin@kabam.com>
2013-12-21 00:59:11 -0800

    Validations on user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEX=/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  
   has_many :posts, dependent: :destroy
+
+  validates :alias, length:{minimum: 3}
+
+  validates_presence_of :email
+  validates :email, length:{minimum: 3}
+  validates_format_of :email, :with=>EMAIL_REGEX, :on=>:create
+  validates_format_of :email, :with=>EMAIL_REGEX, :on=>:update
+  
+  validates_presence_of :name
+  validates :name, length:{minimum: 3}
 end
