8437271b39a3db6372a1139bce434f68445e4ee8
mlin <mlin@kabam.com>
2014-01-13 15:05:08 -0800

    Allow showing users by id as well as nickname

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController  
   def latest
     @post=Post.last
-
     render :show
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,10 @@ class UsersController < ApplicationController
   
   def show
     begin
-      @user=User.find_by_nickname(params[:id])
+      @user||=User.find_by_nickname(params[:id])
+      @user||=User.find(params[:id])
     rescue
       redirect_to latest_path and return
-    end
-
-    respond_to do |format|
-      format.html{ render action: :show }
-      format.js{}
     end
   end
 end
