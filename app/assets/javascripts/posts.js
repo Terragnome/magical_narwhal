8f07b10e0adb2afba2b1649349bf8adb82881367
mlin <mlin@kabam.com>
2014-01-27 00:27:31 -0800

    Share button

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,34 +1,38 @@
 var Post = Post || {};
 
 Post.Init = function(){
-	$('.comment_button').each(function(){ $(this).click(Post.OnCommentButton); });
-	$('.share_button').each(function(){ $(this).click(Post.OnShareButton); });
+	$('.comment_button').each(function(){ $(this).click(Post.OnComment); });
+	$('.share_button').each(function(){ $(this).click(Post.OnShare); });
+	$('.share_close').each(function(){ $(this).click(Post.OnShareClose); });
+	$('.share_copy_link').each(function(){ $(this).click(Post.OnShareCopyLink); });
 }
 
-Post.OnCommentButton = function(){
+Post.OnComment = function(){
 	var commentSection = $(this).parent().parent().find('.fb-comments');
 	Application.ToggleObjDisplay(commentSection);
 }
-
-Post.OnShareButton = function(){ Post.ShowShare($(this).parent().parent(), true); }
-Post.ShowShare = function(postObj){ Post.SetShare(postObj, true); }
+Post.OnShare = function(){ Post.ShowShare($(this).parent().parent()); }
+Post.OnShareClose = function(){ Post.HideShare($(this).parent().parent()); }
+Post.OnShareCopyLink = function(){ $(window).clipboardData.setData("Text", "google.com"); }
 Post.HideShare = function(postObj){ Post.SetShare(postObj, false); }
+Post.ShowShare = function(postObj){ Post.SetShare(postObj, true); }
 Post.SetShare = function(postObj, isOn){
 	var blocker = Application.GetBlocker();
 	Application.SetObjDisplay(blocker, isOn);
 
-	var share = $('#share');
+	var share = $('.share');
 	if(share != null)
 	{
 		if(isOn){
 			share.css('left', ($(document).width()-share.outerWidth())/2+"px");
 			share.css('top', ($(document).height()-share.outerHeight())/2+"px");
-			share.css('display', 'auto');
+			share.css('display', 'block');
 			share.appendTo('#popups');
 
 			blocker.one('click', function(){ Post.HideShare(postObj); });
 		}else{
-			share.appendTo(postObj.find('.share'));
+			share.css('display', 'none');
+			share.appendTo(postObj.find('.share_container'));
 		}
 	}
 }
