acb7b456e04512580ee1849f10c7f8004fd76e42
mlin <mlin@kabam.com>
2014-01-28 14:57:57 -0800

    Created div to help with fades

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,15 +47,15 @@ Application.OnReady = function(){
 }
 
 Application.OnAjaxStart = function(){
-	$('#scene').animate({
+	$('#scene_content').animate({
 		opacity: 0.1
     }, 100);
 }
 Application.OnAjaxComplete = function(){
 	$('html,body').scrollTop(0);
 
-	$('#scene').css('opacity', 0.1);
-	$('#scene').animate({
+	$('#scene_content').css('opacity', 0.1);
+	$('#scene_content').animate({
 		opacity: 1
     }, 100);
 	FB.XFBML.parse();
